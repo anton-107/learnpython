ef8e690262e8ed4d0a5ae2d11f3698444fcce488
Anton Nekipelov <anton.nekipelov@gmail.com>
2016-05-19 13:20:01 +0200

    with statement

---
+++ 008-with-statement.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+
+# with statement will call __enter__ and __exit__ methods on the objects for you automatically,
+# so that you don't need to tear things down in the `finally` clause
+# see more: http://effbot.org/zone/python-with-statement.htm
